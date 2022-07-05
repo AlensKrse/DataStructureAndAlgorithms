6cf1f011792c0f5d02e2e3644bf23223e9ba4e12
alenkrse <alenkrse@gmail.com>
2022-07-05 09:26:29 +0300

    Add shell sort and recursive

---
+++ src/main/Recursive.java
@@ -0,0 +1,43 @@
+package main;
+
+public class Recursive {
+
+    public static void main(String[] args) {
+        final long beforeIterative = System.nanoTime();
+        final int iterativeFactorial = iterativeFactorial(40);
+        final long thenIterative = System.nanoTime();
+
+        System.out.println(thenIterative - beforeIterative);
+
+        final long beforeRecursive = System.nanoTime();
+        final int recursiveFactorial = recursiveFactorial(Integer.MAX_VALUE);
+        final long thenRecursive = System.nanoTime();
+
+        System.out.println(thenRecursive - beforeRecursive);
+
+        System.out.println(iterativeFactorial);
+        System.out.println(recursiveFactorial);
+    }
+
+    public static int recursiveFactorial(int number) {
+        if (number == 0) {
+            return 1;
+        }
+
+        return number * recursiveFactorial(number - 1);
+    }
+
+
+    public static int iterativeFactorial(int number) {
+        if (number == 0) {
+            return 1;
+        }
+
+        int factorial = 1;
+        for (int count = 1; count <= number; count++) {
+            factorial *= count;
+        }
+
+        return factorial;
+    }
+}
--- src/main/ShellSort.java
+++ src/main/ShellSort.java
@@ -2,27 +2,29 @@ package main;
 
 import java.util.Arrays;
 
-public class InsertionSort {
+public class ShellSort {
 
     /*In place algorithm
      * O(N^2) time complexity - quadratic
-     * Stable algorithm
-     * Faster than Bubble sort and Selection sort
-     * ± 19200 nanoseconds*/
+     * Unstable algorithm
+     * Faster than Bubble sort and Selection sort and Insertion sort
+     * ± 15200 nanoseconds*/
 
     public static void main(String[] args) {
         final int[] initialArray = {20, 35, -15, 7, 55, 1, -22, 35, -15, 7, 55, 1, -22, 35, -15, 7, 55, 1, -22, 35, -15, 7, 55, 1, -22, 35, -15, 7, 55, 1, -22, 35, -15, 7, 55, 1, -22, 35, -15, 7, 55, 1, -22, 35, -15, 7, 55, 1, -22, 35, -15, 7, 55, 1, -22, 35, -15, 7, 55, 1, -22, 35, -15, 7, 55, 1, -22, 35, -15, 7, 55, 1, -22};
 
         final long before = System.nanoTime();
-        for (int firstUnsortedIndex = 1; firstUnsortedIndex < initialArray.length - 1; firstUnsortedIndex++) {
-            int newElement = initialArray[firstUnsortedIndex];
+        for (int gap = initialArray.length / 2; gap > 0; gap /= 2) {
+            for (int firstUnsortedIndex = gap; firstUnsortedIndex < initialArray.length; firstUnsortedIndex++) {
+                int newElement = initialArray[firstUnsortedIndex];
 
-            int count;
-            for (count = firstUnsortedIndex; count > 0 && initialArray[count - 1] > newElement; count--) {
-                initialArray[count] = initialArray[count - 1];
+                int count;
+                for (count = firstUnsortedIndex; count >= gap && initialArray[count - gap] > newElement; count -= gap) {
+                    initialArray[count] = initialArray[count - gap];
+                }
+
+                initialArray[count] = newElement;
             }
-
-            initialArray[count] = newElement;
         }
         final long then = System.nanoTime();
 
